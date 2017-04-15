a60a24f8707518ebebf7655735521956b2d12388
Milton Woods <m.woods@bom.gov.au>
2017-04-15 01:26:11 +0000

    spack-env-terra.sh: setup user shell for spack on terra

---
+++ share/spack/spack-env-terra.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Source this file to setup spack shell environment
+
+# Create environment for spack
+export SPACK_FRONT_END=haswell
+export SPACK_BACK_END=haswell
+export SPACK_ROOT=/g/sc/projects_lustre/pr_nwp/mwoods/spack
+
+# Remove compilers from environment:
+module unload PrgEnv-cray PrgEnv-gnu PrgEnv-intel cray gcc intel
+
+# Ensure that modulecmd is in PATH:
+export PATH="$SPACK_ROOT/bin:$PATH:/opt/modules/default/bin"
+
+. "$SPACK_ROOT/share/spack/setup-env.sh"
+
