8d41edd398b30e05ffeece4d6f2941ca5c06ff2f
Milton Woods <milton.woods@bom.gov.au>
2017-04-30 11:57:33 +1000

    qt: add dependencies for opengl support in qt@3

--- var/spack/repos/builtin/packages/qt/package.py
+++ var/spack/repos/builtin/packages/qt/package.py
@@ -104,6 +104,8 @@ class Qt(Package):
 
     # OpenGL hardware acceleration
     depends_on("mesa", when='+mesa')
+    depends_on("mesa-glu", when='@3+mesa')
+    depends_on("libxmu", when='@3+mesa')
 
     # Webkit
     depends_on("flex", when='+webkit', type='build')
@@ -319,6 +321,8 @@ class Qt(Package):
             libs += ['libsm', 'libxext', 'libxinerama', 'libxcursor',
                      'libxrandr', 'randrproto', 'libxrender',
                      'libx11', 'libxft', 'freetype', 'fontconfig']
+        if spec.satisfies('+mesa'):
+             libs += ['mesa', 'mesa-glu', 'libxmu']
         args = map(lambda lib: "-I%s" % spec[lib].prefix.include, libs) \
                + map(lambda lib: "-L%s" % spec[lib].prefix.lib, libs)
         configure('-prefix', self.prefix,
