0ce56d7100b2190f7a435b20d1f2b788dfa99ec6
Milton Woods <milton.woods@bom.gov.au>
2017-10-30 13:10:31 +1100

    lfric: support openmpi

--- var/spack/repos/builtin/packages/lfric/package.py
+++ var/spack/repos/builtin/packages/lfric/package.py
@@ -74,5 +74,8 @@ class Lfric(MakefilePackage):
             spack_env.set('FPP', 'fpp')
         else:
             spack_env.set('FPP', 'ftn -eP')
+        if spec.satisfies('^openmpi'):
+            spack_env.set('OMPI_MCA_orte_execute_quiet', '1')
+            spack_env.append_flags('FFLAGS', '-I%s' % spec['mpi'].prefix.lib)
         spack_env.set('LDMPI', self.spec['mpi'].mpifc)
         spack_env.set('PROFILE', 'fast-debug')
