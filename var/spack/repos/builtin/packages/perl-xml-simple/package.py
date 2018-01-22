09b42ae90fcf45c331b9cb081a596d0c85dab95b
Milton Woods <milton.woods@bom.gov.au>
2018-01-23 10:06:31 +1100

    perl-xml-simple: fix syntax error after merge

--- var/spack/repos/builtin/packages/perl-xml-simple/package.py
+++ var/spack/repos/builtin/packages/perl-xml-simple/package.py
@@ -1,22 +1,13 @@
 ##############################################################################
-<<<<<<< HEAD
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
-=======
 # Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
->>>>>>> 55c3451b87ef5c921c9ff12233b2568f89228cd7
 # Produced at the Lawrence Livermore National Laboratory.
 #
 # This file is part of Spack.
 # Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
 # LLNL-CODE-647188
 #
-<<<<<<< HEAD
-# For details, see https://github.com/llnl/spack
-# Please also see the LICENSE file for our notice and the LGPL.
-=======
 # For details, see https://github.com/spack/spack
 # Please also see the NOTICE and LICENSE files for our notice and the LGPL.
->>>>>>> 55c3451b87ef5c921c9ff12233b2568f89228cd7
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License (as
