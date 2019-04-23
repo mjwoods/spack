14e872c017223c244140ffd5e24f900eb4df5c71
Milton Woods <m.woods@bom.gov.au>
2019-04-23 01:49:49 +0000

    terra: update yaml files for bom-develop3

--- share/spack/spack-env-terra.sh
+++ share/spack/spack-env-terra.sh
@@ -9,6 +9,9 @@ export SPACK_ROOT="${SPACK_ROOT-/g/sc/projects_lustre/pr_rdshare/spack}"
 # Remove compilers from environment:
 module unload PrgEnv-cray PrgEnv-gnu PrgEnv-intel cray gcc intel
 
+# Use a newer python:
+export PATH=/g/sc/apps_dev/A1.2.0/python/2.7.5/bin:$PATH
+
 . "$SPACK_ROOT/share/spack/setup-env.sh"
 
 umask 0002
